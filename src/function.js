34f2d3e523f445926a28dbeee291599d6bfd6799
Mufurun <ovis1gmelini@gmail.com>
2025-12-18 09:26:00 -0800

    goos

--- src/function.js
+++ src/function.js
@@ -1503,6 +1503,7 @@ else if(region =='TumblerRidge'){list.innerHTML = 'List of Sites in Tumbler Ridg
             </div></div>
           </div>
             `;//              <label><input type="checkbox" id="BCFS" checked>Show BCFS</label><br>
+            // If you uncomment this, do so in solid_data.js ('BCFS')
 
       }
 /*
